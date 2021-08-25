28d504ecea92635882285e5769ead47c43f660e1
muchtaragung <85227816+muchtaragung@users.noreply.github.com>
2021-08-25 17:15:32 +0700

    Update Main.php

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -166,10 +166,9 @@ class Main extends CI_Controller
                 'mailtype'  => 'html',
                 'charset'   => 'utf-8',
                 'protocol'  => 'smtp',
-                'smtp_host' => 'smtp.gmail.com',
+                'smtp_host' => 'tls://smtp.gmail.com',
                 'smtp_user' => 'moneytypeq@gmail.com',  // Email gmail
                 'smtp_pass'   => 'Money12p',  // Password gmail
-                'smtp_crypto' => 'ssl',
                 'smtp_port'   => 465,
                 'crlf'    => "\r\n",
                 'newline' => "\r\n"
