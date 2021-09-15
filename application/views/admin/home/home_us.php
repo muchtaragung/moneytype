ca38281b0d21a4886ba15e381caadaa8768ba83f
muchtaragung <muchtaragung7@gmail.com>
2021-09-15 10:04:18 +0700

    ganti US to EN

--- application/views/admin/home/home_us.php
+++ application/views/admin/home/home_us.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Home US</h1>
+                    <h1 class="m-0">Home EN</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -63,7 +63,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Edit Home US</h5>
+                <h5 class="modal-title">Edit Home EN</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
