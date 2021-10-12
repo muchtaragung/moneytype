43907ffd43b0ab08621965cf6e0d06ec463a64aa
muchtaragung <muchtaragung7@gmail.com>
2021-10-12 11:20:13 +0700

    file forgot password

---
+++ application/views/admin/forget_password.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vidira Forgot Password</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() ?>assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url() ?>assets/favicon/site.webmanifest">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/admin/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/admin/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/admin/adminlte.min.css">
+    <script src="<?= base_url() ?>assets/admin/jquery/jquery.min.js"></script>
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="card card-outline card-warning">
+            <div class="card-header text-center">
+                <img width="200px" src="<?= base_url() ?>assets/admin/assets/logo/<?= $logo->logo_footer ?>" class="brand-image elevation-3" style="opacity: .8">
+                <h3 class="mt-4">Lupa Password</h3>
+                <?php if ($this->session->flashdata('error') != null) { ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $this->session->flashdata('error'); ?>
+                    </div>
+                <?php } ?>
+                <?php if ($this->session->flashdata('msg') != null) { ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo $this->session->flashdata('msg'); ?>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
+                <form action="<?= base_url() ?>admin/login/lupa_password" method="post">
+                    <?= form_error('email') ?>
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">Request new password</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+                <p class="mt-3 mb-1">
+                    <a href="<?= base_url() ?>admin/login">Login</a>
+                </p>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+
+
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url() ?>assets/admin/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url() ?>assets/admin/adminlte.min.js"></script>
+</body>
+
+</html>
