88a4dc77e8c240dac45918bc6011a94ef95c13f4
muchtaragung <muchtaragung7@gmail.com>
2021-10-25 14:42:57 +0700

    fix home

--- application/views/web/finish/us.php
+++ application/views/web/finish/us.php
@@ -3,7 +3,9 @@
     <div class="mask h-100 w-100" style="background-color: rgba(0, 0, 0, 0.6);">
         <div class="d-flex justify-content-center align-items-center h-100">
             <div class="text-white">
-                <h1 class="mb-3 text-white">Thank you, Please check your email for result</h1>
+                <h1 class="mb-3 text-white">Thank you, Please check your email for result.</h1>
+                <h3 class="mb-3 text-white">If you have not yet received an email, check your Spam folder, or try again.</h3>
+
                 <!-- <a class="btn btn-warning btn-lg" href="<?= base_url() ?>moneytype" role="button">Money Type Quiz</a> -->
             </div>
         </div>
--- application/views/web/home/us.php
+++ application/views/web/home/us.php
@@ -146,11 +146,6 @@
         </nav>
 
         <div id="Vidira" class="carousel slide carousel-fade" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#Vidira" data-slide-to="0" class="active"></li>
-                <li data-target="#Vidira" data-slide-to="1"></li>
-                <li data-target="#Vidira" data-slide-to="2"></li>
-            </ol>
             <div class="carousel-inner" role="listbox">
                 <?php
                 $i = 1;
